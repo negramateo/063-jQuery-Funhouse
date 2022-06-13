55c532f7b02b2cc3ae0fcffc9605d87428e00d6f
negramateo <107415426+negramateo@users.noreply.github.com>
2022-06-13 10:00:08 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball   ").click(function() {
+    $(".pikachu").fadeIn();
+    
+});
+$(".lightt-switch").click(function() {
+$(".light-blub  ").toggle();
+
+});
